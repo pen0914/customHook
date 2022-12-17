6dd54f3d0e677851925cc8f438c529c79c271581
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-12-17 16:32:36 +0900

    カスタムフック

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
 import "./styles.css";
-import axios from "axios";
 import { UserCard } from "./components/UserCard";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/UserProfile";
-import { useState } from "react";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickData = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickData = () => getUsers();
 
   return (
     <div className="App">
